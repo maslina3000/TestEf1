ade4d8a75ef0127a2f5cc953d12a34fb57ad06d1
Andrey <40323707+maslina3000@users.noreply.github.com>
2019-07-07 12:08:43 +0300

    123

---
+++ TestEfsol123/Test/Models/AnswerForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test.Models
+{
+    public class AnswerForm
+    {
+       // public Guid SessionID { get; set; }
+       // public string Caption { get; set; }
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+       /* {
+            f,
+            m
+        }
+        */
+        public DateTime Birth { get; set; }
+        public string Marital { get; set; }    
+        /*  {
+            m,
+            s
+        }
+        */
+        public bool Programming { get; set; }
+
+    }
+}
---
+++ TestEfsol123/Test/Models/AnswerRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test.Models
+{
+    public class AnswerRepository
+    {
+        private static AnswerRepository repository = new AnswerRepository();
+        private List<AnswerForm> responses = new List<AnswerForm>();
+        private static AnswerRepository GetRepository()
+        {
+            return repository;
+        }
+        public IEnumerable<AnswerForm> GetAllResponses()
+        {
+            return responses;
+        }
+     /*   public void AddResponse(AnswerForm response)
+        {
+            response.Add(response);
+        }
+*/
+    }
+}
